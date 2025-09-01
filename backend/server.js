8075a8524650063f38453fb495daef073248c35c
huywb <pthhuy1306@gmail.com>
2025-09-01 19:07:14 +0700

    server Setup

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+dotenv.config()
+const app = express()
+const PORT = process.env.PORT || 4000
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+
+
+app.use("/api/user",(req,res)=>{
+    res.status(200).json({message:"User Router"})
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
+
+
